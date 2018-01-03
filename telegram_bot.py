f674f19dd55862a97f51409cf07c72446df9827d
Pavel Zagalsky <zagalsky@gmail.com>
2018-01-03 12:09:04 +0200

    Fixes

--- telegram_bot.py
+++ telegram_bot.py
@@ -89,7 +89,6 @@ def main():
             last_update_id = get_last_update_id(updates) + 1
             command, chat_id = get_last_chat_id_and_text(updates)
             send_mail(send_to=from_address, subject=command)
-            send_message(msg, chat_id)
             if command == 'status':
                 msg = ('{} {}'.format('Generator status is:', get_gen_state()))
                 send_message(msg, chat_id)
