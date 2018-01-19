e9f7d30ce8f70541a7c98fb7d55de29445c2ec10
pavelzag <zagalsky@gmail.com>
2018-01-19 22:57:20 +0200

    Fixes for the pics and statuses for timed execution

--- telegram_bot.py
+++ telegram_bot.py
@@ -127,7 +127,14 @@ def wait_for_interrupt(run_time, interrupt):
         if len(updates["result"]) > 0:
             last_update_id = get_last_update_id(updates) + 1
             key_command, chat_id = get_last_chat_id_and_text(updates)
-            send_mail(send_to=gen_email, subject=key_command)
+            if key_command == 'pic':
+                pic_command()
+                send_image(chat_id)
+            elif key_command == 'status':
+                msg = ('{} {}'.format('Generator status is:', get_gen_state()))
+                send_message(msg, chat_id)
+            else:
+                send_mail(send_to=gen_email, subject=key_command)
             if key_command.lower() == 'off':
                 interrupt = True
     if not interrupt:
@@ -135,7 +142,7 @@ def wait_for_interrupt(run_time, interrupt):
         time_spent = ' '.join(get_last_time_spent())
         msg = '{} {} {}'.format(telegram_success_msg, 'Generator is going down. Generator was up for',
                                 time_spent)
-        send_image(msg, chat_id)
+        send_message(msg, chat_id)
     return interrupt
 
 
@@ -155,7 +162,8 @@ def main():
                 send_message(msg, chat_id)
             elif str(chat_id) in white_list:
                 key_command = key_command.lower()
-                send_mail(send_to=gen_email, subject=key_command)
+                if 'on' in key_command or 'off' in key_command:
+                    send_mail(send_to=gen_email, subject=key_command)
                 if key_command == 'status':
                     msg = ('{} {}'.format('Generator status is:', get_gen_state()))
                     send_message(msg, chat_id)
